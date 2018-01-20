1d142948e78ee8efc5346a8ff94a6c8e124d0ce8
Khoi <kntran10@gmail.com>
2018-01-20 14:55:16 -0500

    Gulp file set up with browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,55 @@
+var srcFiles = './app/'
+var distFiles = './dist/'
+
 var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
-    sass = require('gulp-sass')
+    sass = require('gulp-sass'),
+    sequence = require('gulp-sequence'),
+    browserSync = require('browser-sync').create()
 
 gulp.task('sass', function(){
-    return gulp.src('./sass/**/*.scss')
-        .pipe(sass().on('error', sass.logError()))
+    return gulp.src(srcFiles + '**/*.scss')
+        .pipe(sass().on('error', function(e){
+            console.log(e);
+        }))
         .pipe(gulp.dest('./dist/'))
+})
+
+gulp.task('html',function(){
+    return gulp.src(srcFiles + '**/*.html')
+        .pipe(gulp.dest(distFiles));
+})
+
+gulp.task('javascript', function(){
+    return gulp.src(srcFiles + '**/*.js')
+        .pipe(gulp.dest(distFiles))
+})
+
+gulp.task('css', function(){
+    return gulp.src(srcFiles + '**/*.css')
+        .pipe(gulp.dest(distFiles))
+})
+
+gulp.task('all',function(){
+    return gulp.src(srcFiles + '**/!(*.html | *.js | *.css | *.scss)')
+})
+
+gulp.task('serve', function(){
+    browserSync.init({
+        host:'localhost',
+        server:distFiles,
+        open:false
+    })
+
+    sequence(['sass','css','html','javascript'],'all', function(){
+        gulp.watch(srcFiles + '**/*').on('change', function(){
+            sequence(['sass','css','html','javascript'],'all', function(){
+                browserSync.reload();
+            })
+        })
+    })
+})
+
+gulp.task('build', function(){
+    sequence(['sass','css','html','javascript'],'all')
 })
